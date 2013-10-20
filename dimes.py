99fb0a63b16c8445dc2456ed7bf68b2caaf49f7e
Cameron-D <camerondew@live.com>
2013-10-20 22:33:31 +1000

    Removed excessive debug info

--- dimes.py
+++ dimes.py
@@ -9,7 +9,7 @@ script.execute()
 try:
     while threading.active_count() > 1:
         time.sleep(0.1)
-        Log.log("%d pending, %d complete" % (script.operationQ.qsize(), script.resultQ.qsize()))
-    Log.log("All done, exiting!", 3)
+        #Log.log("%d pending, %d complete" % (script.operationQ.qsize(), script.resultQ.qsize()))
+    Log.log("All done, exiting!", 2)
 except KeyboardInterrupt:
-    Log.log("Received Ctrl+C, ending %d threads... (This may take a moment)" % threading.active_count(), 3)
+    Log.log("Received Ctrl+C, ending %d threads... (This may take a moment)" % threading.active_count(), 2)
